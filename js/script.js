79c1667e5c50f9af578abf5ea8862f62fe849140
batrickswaistan <batrickswaistan@gmail.com>
2022-09-15 15:32:31 +0530

    Prtfolio website project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============== typing animation ===================== */
+var typed = new Typed(".typing",{
+  strings:["","Python Developer","Web Developer","Data Miner"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
